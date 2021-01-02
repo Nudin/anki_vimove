1e179e7a0d3f666a9f17a678005ef7a54922ef48
Michael F. Schönitzer <michael@schoenitzer.de>
2021-01-02 03:08:44 +0100

    Add type hints and rename README

--- __init__.py
+++ __init__.py
@@ -1,3 +1,5 @@
+from typing import Callable, Iterable, NewType, Optional
+
 from aqt import mw
 
 # TODO better navigate in tree
@@ -5,25 +7,29 @@ from aqt import mw
 # mw.col.decks.find_deck_in_tree(tree, 1607387237528)
 
 
+# Id of a deck (calld 'did' in Anki code)
+DeckId = NewType("DeckId", int)
+
+# Trivial helper class for cleaner code
 class NoneId:
     id = None
 
 
-def get_first(all_decks, _):
+def get_first(all_decks: Iterable, _) -> Optional[DeckId]:
     try:
         return next(iter(all_decks)).id
     except StopIteration:
         return None
 
 
-def get_last(all_decks, _):
+def get_last(all_decks: Iterable, _) -> Optional[DeckId]:
     last = NoneId
     for last in all_decks:
         pass
     return last.id
 
 
-def get_next(all_decks, current):
+def get_next(all_decks: Iterable, current: DeckId) -> Optional[int]:
     found = False
     next_elem = NoneId
     for deck in all_decks:
@@ -35,7 +41,7 @@ def get_next(all_decks, current):
     return next_elem.id
 
 
-def get_previous(all_decks, current):
+def get_previous(all_decks: Iterable, current: DeckId) -> Optional[int]:
     last = NoneId
     for deck in all_decks:
         if deck.id == current:
@@ -44,12 +50,16 @@ def get_previous(all_decks, current):
     return last.id
 
 
-def goto(target):
+def goto(target: DeckId):
+    if mw is None:
+        raise RuntimeError("Can't get interface. Anki not running?")
     mw.col.decks.select(target)
     mw.deckBrowser.show()
 
 
-def move(func):
+def move(func: Callable):
+    if mw is None:
+        raise RuntimeError("Can't get interface. Anki not running?")
     current = mw.col.decks.selected()
     alldecks = mw.col.decks.all_names_and_ids()
     new_deck_id = func(alldecks, current)
@@ -72,6 +82,9 @@ def goto_last_deck():
 def goto_previous_deck():
     move(get_previous)
 
+
+if mw is None:
+    raise RuntimeError("Can't get interface. Anki not running?")
 
 mw.applyShortcuts([("j", goto_next_deck)])
 mw.applyShortcuts([("k", goto_previous_deck)])
