71f4584a2824c4ad87e6054e4da7971fd18a8985
Michael F. Schönitzer <michael@schoenitzer.de>
2021-06-08 02:26:03 +0200

    Fix: add config.json to package

--- pack.sh
+++ pack.sh
@@ -3,4 +3,4 @@
 echo -n '{"package": "vimove", "name": "vi like movements", "mod": ' > manifest.json
 date +%s >> manifest.json
 echo '}' >> manifest.json
-zip vimove.ankiaddon ./*py manifest.json
+zip vimove.ankiaddon ./*py manifest.json config.json
